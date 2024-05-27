9621da70a55d3c8c4490cfb0e495ed8643b9f4fa
Julio Machado <jmachado@vtsystems.cl>
2024-05-26 20:50:41 -0400

    Actualizacion

--- link_bio/link_bio/views/links/links.py
+++ link_bio/link_bio/views/links/links.py
@@ -33,33 +33,6 @@ def links() -> rx.Component:
             "linkedin"
         ),
         title(
-            "Personales"
-        ),
-        link_button(
-            "Twitch",
-            "Canal de Twitch",
-            "https://www.twitch.tv/juliomv93",
-            "twitch"
-        ),
-        link_button(
-            "Youtube",
-            "Canal de YouTube",
-            "https://www.youtube.com/channel/UCaRhTIsmEQYwkrkb_iHmt9A",
-            "youtube"
-        ),
-        link_button(
-            "github",
-            "Repositorio de GitHub",
-            "https://github.com/JulioV93",
-            "github"
-        ),
-        link_button(
-            "Linkedin",
-            "Perfil de Linkedin",
-            "https://www.linkedin.com/in/julio-alejandro-machado/",
-            "linkedin"
-        ),
-        title(
             "Contacto"
         ),
         link_button(
